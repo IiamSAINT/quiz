bd471fd52552080ef3714e4b01b3fa931d9a7479
IiamSAINT <omotoshoopeyemi2007@gmail.com>
2024-12-11 05:12:32 -0800

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,105 @@
+# #FOR LOOPS
+# # range would show the scope of your count
+# count = 0
+# for x in range(1,11, 2):
+#     count += x
+#
+# print(count)
+# # You can iterate over a string also
+# # Continue keyword to skip over an iteration
+#
+# word = "I Love You"
+#
+# for x in range(101) :
+#     print(word)
+from mimetypes import guess_type
+
+# dir
+# help function
+
+
+# RECCURSION
+#   quiz game
+# 2D Lists /Tupples
+# Create a Keypad
+# Create a Quiz Game
+#  Who Painted the Famous artwork "The Starry Night"?
+# A Leonardo da Vinci B. Vincent van Gogh C. Pablo Picasso D. Claude Monet  Correct: B
+# What is the largest Planet in our solar system?
+# A. Earth b. Saturn C. Jupiter D. Uranus  Correct: C
+# What does the Acronym "API" stand for in computer?
+ # A. Application Proramming Interface B. Artificial Programming Interface C. Advanced Programming Interface D. Automated Programming Interface
+#  How many Bones are in the Human Body ?
+# A. 206 B. 207 C. 208 D. 209
+
+questions = (
+    '1. Who Painted the Famous artwork "The Starry Night"?',
+    '2. What is the largest Planet in our solar system?',
+    '3. What does the Acronym "API" stand for in computer?',
+    '4. How many Bones are in the Human Body ?',
+    '5. Which Planet is the Hottest in the Solar System? '
+)
+options = (
+    (
+    "A. Leonardo da Vinci ",
+    "B. Vincent van Gogh ",
+    "C. Pablo Picasso ",
+    "D. Claude Monet"
+    ),
+    (
+    "A. Earth ",
+    "b. Saturn ",
+    "C. Jupiter ",
+    "D. Uranus"
+    ),
+    (
+    "A. Application Programming Interface ",
+   "B. Artificial Programming Interface ",
+   "C. Advanced Programming Interface",
+    "D. Automated Programming Interface"
+    ),
+    (
+    "A. 206 ",
+    "B. 207 ",
+    "C. 208 ",
+   "D. 209"
+    ),
+(
+    "A. Jupiter ",
+    "B. Mercury ",
+    "C. Venus ",
+   "D. Mars"
+    )
+)
+Answers = [
+    "B", "C", "A", "A", "C"
+]
+guesses = []
+score = 0
+itt_index = 0
+
+for question in questions:
+    print("------------------------------------------------------")
+    print(question)
+    for option in options[itt_index]:
+        print(option)
+
+    guess = input("Enter Your Answer(A,B,C,D) : ")
+    guesses.append(guess)
+    if guess.upper() == Answers[itt_index]:
+        print("CORRECT")
+        score += 1
+    else: print(f"INCORRECT \n{Answers[itt_index]} is the Correct Answer")
+
+
+    itt_index +=1
+print("\n")
+print("---------------------------------------------")
+print("                 SCORE                       ")
+print("---------------------------------------------")
+
+print(f" CORRECT ANSWERS : {Answers}")
+print(f" YOUR GUESSES: {guesses}")
+T_score = int((score / len(Answers)) * 100)
+print(f"Your Total Score is {T_score}%")
+
